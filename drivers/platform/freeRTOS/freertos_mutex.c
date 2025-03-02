227315e9ba969121948cf5fee73ffa4f06bdac1d
Ciprian Regus <ciprian.regus@analog.com>
2025-03-03 00:07:56 +0200

    new line

--- drivers/platform/freeRTOS/freertos_mutex.c
+++ drivers/platform/freeRTOS/freertos_mutex.c
@@ -31,6 +31,7 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
+
 #include <FreeRTOS.h>
 #include "no_os_mutex.h"
 #include "semphr.h"
